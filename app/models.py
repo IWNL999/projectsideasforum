21778e961fafd1af99c5a7cb3529c81216a3404a
IWNL999 <153728057+IWNL999@users.noreply.github.com>
2023-12-17 17:15:50 +0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,9 +1,19 @@
+import os
+from flask import current_app
+from werkzeug.datastructures import FileStorage
+
 from app import db, bcrypt
-from flask import Flask
+from werkzeug.utils import secure_filename
 from datetime import datetime
 from flask_login import UserMixin
 from flask_bcrypt import check_password_hash
-from sqlalchemy.exc import OperationalError
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 class User(UserMixin, db.Model):
@@ -14,11 +24,26 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(60), nullable=False, unique=True)
     file = db.Column(db.String(255))
 
-    def __init__(self, login, password, email, file):
+    def __init__(self, login, password, email, file=None):
         self.login = login
         self.set_password(password)
         self.email = email
-        self.file = file
+        if file:
+            # Проверяем, является ли file объектом файла, прежде чем вызывать save_file
+            if isinstance(file, FileStorage):
+                self.file = self.save_file(file)
+            else:
+                self.file = file
+        else:
+            self.file = None
+
+    def save_file(self, file):
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            return filename  # Возвращаем только имя файла
+        return None
 
     def is_active(self):
         return True
@@ -34,6 +59,8 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
 
 
 class Article(db.Model):
